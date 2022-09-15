f44cc5ef2a13ce40f2f8a15e23dac7ce94ff7146
canb0 <canberkakduygu@gmail.com>
2022-09-15 10:16:39 +0200

    Update wdio.conf.ts

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -86,7 +86,7 @@ export const config: Options.Testrunner = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'firefox',
+        browserName: 'chrome',
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
